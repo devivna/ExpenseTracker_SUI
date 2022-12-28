7109fb759817bc8d9d39ea69b5090499a9aa3de0
Nadia <ivashchenko.nadia@gmail.com>
2022-12-28 19:57:56 +0200

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Students on 28.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
